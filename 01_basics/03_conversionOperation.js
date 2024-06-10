cb4e30d00de96d9c117c11634b26a20bf34795e8
sujal-2311 <123873901+sujal-2311@users.noreply.github.com>
2024-06-10 07:27:01 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,21 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ----------------------Opeartions--------------------------------------
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " sujal"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 +"2");
+
+console.log(+true);
+console.log(+"");
